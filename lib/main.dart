133bf156b4dc4487491e514c66fd5ba86212378e
Adrian Robles <adrianrobles605@gmail.com>
2025-07-23 19:59:56 -0700

    Pantalla de inicio de sesion casi terminada con una animacion de entrada, tambien puse que cuando se abra el teclado se ajuste el contenido

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0x5E22BE10), // color base
+          primary: Color(0xFF036B17),   // color primario personalizado
+          secondary: Color(0xFFFFFFFF), // color secundario personalizado
+        ),
+        useMaterial3: true, // si estás usando Material 3
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: Color(0xFF78BB6F),
+          primary: Color(0xFF154B19),
+          secondary: Color(0xFFFFFFFF),
+        ),
+      ),
+      themeMode: ThemeMode.system, // cambia según modo claro/oscuro del sistema
       title: 'Punto de Venta',
       home: LoginScreen(),
     );
